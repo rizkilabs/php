ce15445be74993f122a2672a9605c7f4a141c1f9
fifinazh <fitrianurzh01@gmail.com>
2025-03-04 09:52:13 +0700

    add connection to mysql database

---
+++ hari_16/koneksi.php
@@ -0,0 +1,16 @@
+<?php 
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "belajar_php";
+
+
+$mysqli = new mysqli($host, $user, $pass, $db);
+
+if($mysqli->connect_error){
+    die("Koneksi gagal: " . $mysqli->connect_error);
+} else {
+    echo "Koneksi berhasil";
+}
+
+?>
